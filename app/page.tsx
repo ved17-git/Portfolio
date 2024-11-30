9c5e14599692e57609e3815be51d997111e56661
Da4Rkclown <118552956+Da4Rkclown@users.noreply.github.com>
2024-11-30 23:34:29 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
+  return (<> 
+    
+
+    <h1>dawg</h1>
+    
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
@@ -97,5 +101,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
+    </> );
 }
